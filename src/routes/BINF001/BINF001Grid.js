a8c2b164c8be8cae1c28dbb0022c29a84c6e9c99
checks01 <isc.sva@gmail.com>
2020-01-15 19:29:40 -0600

    avance desktop

--- src/routes/BINF001/BINF001Grid.js
+++ src/routes/BINF001/BINF001Grid.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MaterialTable from 'material-table';
+import axios from 'axios';
 
 export default function BINF001Grid() {
   const [state, setState] = React.useState({
     columns: [
-      { title: 'ID usuario', field: 'idUsuario', hidden:true },
+      { title: 'ID usuario', field: 'idUsuario', hidden: true },
       { title: 'Clave usuario', field: 'cveUsuario' },
       { title: 'Password', field: 'password' },
       { title: 'Nombre', field: 'nombre' },
@@ -14,57 +15,78 @@ export default function BINF001Grid() {
         field: 'activo',
         lookup: { 'S': 'Si', 'N': 'No' },
       },
-      { title: 'Usuario', field: 'usuario', editable: false},
-      { title: 'Fecha actualización', field: 'fechaActualizacion', editable: false}
+      { title: 'Usuario', field: 'usuario', editable: false },
+      { title: 'Fecha actualización', field: 'fechaActualizacion', editable: false }
     ],
-    data: [
-      { idUsuario: 1, cveUsuario: '000001', password: '123.Hola', nombre: 'Salvador Antonio Ayala Chávez', email: 'sayala002@accitesz.com', activo: 'S', usuario: 'ADMIN', fechaActualizacion: '13 Enero 2019 13:13:22'},
-      { idUsuario: 2, cveUsuario: '000002', password: '123.Hola', nombre: 'Sergio Valle Alarcón', email: 'svalle016@accitesz.com', activo: 'S', usuario: 'ADMIN', fechaActualizacion: '13 Enero 2019 13:20:01'},
-    ],
+    data: [],
   });
 
-  return (
-    <MaterialTable
-      title="Catálogo de usuarios"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: newData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              setState(prevState => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState(prevState => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: oldData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              setState(prevState => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-      }}
-    />
-  );
+  class BINF001Grid extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { state };
+    }
+
+    componentDidMount() {
+      console.log("peticion")
+      axios.get('http://localhost:8085/usuarios')
+        .then(response => {
+          console.log('respuesta')
+          state.data = response.data._embedded.usuarioses;
+          this.setState({ state });
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+    render() {
+      return (
+        <MaterialTable
+          title="Catálogo de usuarios"
+          columns={state.columns}
+          data={state.data}
+          editable={{
+            onRowAdd: newData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  setState(prevState => {
+                    const data = [...prevState.data];
+                    console.log('datos previos: ' + data);
+                    data.push(newData);
+                    console.log('nuevos datos: ' + newData)
+                    return { ...prevState, data };
+                  });
+                }, 600);
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  if (oldData) {
+                    setState(prevState => {
+                      const data = [...prevState.data];
+                      data[data.indexOf(oldData)] = newData;
+                      return { ...prevState, data };
+                    });
+                  }
+                }, 600);
+              }),
+            onRowDelete: oldData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  setState(prevState => {
+                    const data = [...prevState.data];
+                    data.splice(data.indexOf(oldData), 1);
+                    return { ...prevState, data };
+                  });
+                }, 600);
+              }),
+          }}
+        />
+      );
+    }
+  }
+  return <BINF001Grid />
 }
